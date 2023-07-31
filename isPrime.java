1e01856f9790486f10f506e3cc8ae56917fc2489
steveAzo <135361078+steveAzo@users.noreply.github.com>
2023-07-31 14:57:23 +0000

    added isPrimeChecker function to check if the number is  a prime number or not

--- isPrime.java
+++ isPrime.java
@@ -2,6 +2,16 @@ package isPrime;
 
 public class isPrime {
 
+	public boolean isPrimeChecker(int num) {
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
